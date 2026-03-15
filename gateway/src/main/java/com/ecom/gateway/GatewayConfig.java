a9d0fb75c52e906344223d5b9fec4980a9a08cd3
Priyanshu Chouhan <priynshuchouhn@gmail.com>
2026-03-15 19:43:39 +0530

    redis rate limiting added

--- gateway/src/main/java/com/ecom/gateway/GatewayConfig.java
+++ gateway/src/main/java/com/ecom/gateway/GatewayConfig.java
@@ -1,13 +1,28 @@
 package com.ecom.gateway;
 
+import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
+import org.springframework.cloud.gateway.filter.ratelimit.RedisRateLimiter;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import reactor.core.publisher.Mono;
 
 @Configuration
 public class GatewayConfig {
+
+    @Bean
+    public RedisRateLimiter redisRateLimiter() {
+        return new RedisRateLimiter(10,20,1);
+    }
+
+    @Bean
+    public KeyResolver keyResolver() {
+        return exchange -> Mono.just(exchange.getRequest()
+                .getRemoteAddress().getHostName());
+    }
+
     @Bean
     public RouteLocator routeLocator(RouteLocatorBuilder builder) {
         return builder.routes()
@@ -17,6 +32,9 @@ public class GatewayConfig {
                                 .retry(retryConfig -> retryConfig
                                         .setRetries(10)
                                         .setMethods(HttpMethod.GET))
+                                .requestRateLimiter(config -> config
+                                        .setRateLimiter(redisRateLimiter())
+                                        .setKeyResolver(keyResolver()))
                                 .circuitBreaker(config -> config
                                 .setName("ecomBreaker")
                                 .setFallbackUri("forward:/fallback/product")))
